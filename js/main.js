01d1ea9ce2e0bb0c0079273956b39ed4f0fac363
elid venega <venegaelid@gmail.com>
2020-09-10 20:23:33 -0700

    function mastery

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// I can only use function
+// purpose get better with functions
+function addText() {
+    let x = "Hello I'am programming";
+    let y = "at work and it works.";
+    return `${x} ${y}`;
+}
+
+document.getElementById("id").innerHTML = addText();
+document.getElementById("id").style.fontSize = '40px';
+
+// second function
+function addInfo(name, lastName, age, address) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.address = address;
+    this.info = function () {
+        return `${name} ${lastName} ${age} ${address}`
+    }
+};
+
+let sum = new addInfo('Jeff','Bezos' , 54, 'washington');
+
+
+document.getElementById("info").innerHTML = sum.info();
